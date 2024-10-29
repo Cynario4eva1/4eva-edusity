19da7418e35a5bb25f2c6e0b30ca98a0e7de2519
Cynario4eva <joshdela233@gmail.com>
2024-10-28 17:35:28 -0700

    first commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <p>Design by <span>Cynario4eva</span></p>
+      <ul>
+        <li>Terms of Service</li>
+        <li>Privacy Policy</li>
+      </ul>
+      <p>© 2024 Edusity. All rights reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
